bfcb90ed3c4782261fce14766496d88a15e677a4
Tim Schindler <xh3b4sd@gmail.com>
2018-08-20 11:21:11 +0200

    initialized daemon with microkit commands (#3)

--- main.go
+++ main.go
@@ -1,3 +1,121 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/microkit/command"
+	microserver "github.com/giantswarm/microkit/server"
+	"github.com/giantswarm/micrologger"
+	"github.com/spf13/viper"
+
+	"github.com/giantswarm/node-health/flag"
+	"github.com/giantswarm/node-health/server"
+	"github.com/giantswarm/node-health/service"
+)
+
+var (
+	description string     = "Kubernetes daemonset for dynamic and composable node health management."
+	f           *flag.Flag = flag.New()
+	gitCommit   string     = "n/a"
+	name        string     = "node-health"
+	source      string     = "https://github.com/giantswarm/node-health"
+)
+
+func main() {
+	err := mainError()
+	if err != nil {
+		panic(fmt.Sprintf("%#v\n", err))
+	}
+}
+
+func mainError() error {
+	var err error
+
+	// Create a new logger which is used by all packages.
+	var newLogger micrologger.Logger
+	{
+		c := micrologger.Config{}
+
+		newLogger, err = micrologger.New(c)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	// We define a server factory to create the custom server once all command
+	// line flags are parsed and all microservice configuration is storted out.
+	newServerFactory := func(v *viper.Viper) microserver.Server {
+		// Create a new custom service which implements business logic.
+		var newService *service.Service
+		{
+			c := service.Config{
+				Logger: newLogger,
+
+				Description: description,
+				Flag:        f,
+				GitCommit:   gitCommit,
+				Name:        name,
+				Source:      source,
+				Viper:       v,
+			}
+
+			newService, err = service.New(c)
+			if err != nil {
+				panic(fmt.Sprintf("%#v", err))
+			}
+			go newService.Boot()
+		}
+
+		// Create a new custom server which bundles our endpoints.
+		var newServer microserver.Server
+		{
+			c := server.Config{
+				Logger:  newLogger,
+				Service: newService,
+				Viper:   v,
+
+				ProjectName: name,
+			}
+
+			newServer, err = server.New(c)
+			if err != nil {
+				panic(fmt.Sprintf("%#v", err))
+			}
+		}
+
+		return newServer
+	}
+
+	// Create a new microkit command which manages our custom microservice.
+	var newCommand command.Command
+	{
+		c := command.Config{
+			Logger:        newLogger,
+			ServerFactory: newServerFactory,
+
+			Description:    description,
+			GitCommit:      gitCommit,
+			Name:           name,
+			Source:         source,
+			VersionBundles: nil,
+		}
+
+		newCommand, err = command.New(c)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	daemonCommand := newCommand.DaemonCommand().CobraCommand()
+
+	daemonCommand.PersistentFlags().String(f.Service.Kubernetes.Address, "http://127.0.0.1:6443", "Address used to connect to Kubernetes. When empty in-cluster config is created.")
+	daemonCommand.PersistentFlags().Bool(f.Service.Kubernetes.InCluster, false, "Whether to use the in-cluster config to authenticate with Kubernetes.")
+	daemonCommand.PersistentFlags().String(f.Service.Kubernetes.TLS.CAFile, "", "Certificate authority file path to use to authenticate with Kubernetes.")
+	daemonCommand.PersistentFlags().String(f.Service.Kubernetes.TLS.CrtFile, "", "Certificate file path to use to authenticate with Kubernetes.")
+	daemonCommand.PersistentFlags().String(f.Service.Kubernetes.TLS.KeyFile, "", "Key file path to use to authenticate with Kubernetes.")
+
+	newCommand.CobraCommand().Execute()
+
+	return nil
+}
